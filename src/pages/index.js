83527539ceeb1a1ddea795bce7569f9ed6ebd7e8
JoseOgam <jotienoochieng@gmail.com>
2020-03-19 17:24:59 +0300

    finally index page css working

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,31 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import portfolioItems from "../components/portfolio-items"
-import "../styles/index.css"
+import "../styles/index.styles.css"
 
 
 function renderFolio() {
   return portfolioItems.map(({ title, sinopsis, idx, desc, repo, live, img }) => {
     console.log(title)
     return (
-
-
       <div key={"div" + idx} className="tab">
         <input key={"index" + idx} type="checkbox" id={idx + title}/>
-        <label key={"label" + idx} className="tab-label" htmlFor={idx + title}> <span key={"pTit" + idx}
-                                                                                  className='folio-title'>{title} </span>
+        <label key={"label" + idx} className="tab-label" htmlFor={idx + title}>
+          <span key={"pTit" + idx}
+                className='folio-title'>{title} </span>
           <span key={"dot" + idx} className='folio-title'>· </span>
           <span key={"pSinop" + idx} className='folio-sinopsis'>{sinopsis}</span></label>
         <div key={"tab" + idx} className="tab-content">
           <img alt="nicasia" src={img}/>
           {desc}
-          <span className="repolinks" key={"repo" + idx}><a key={"repoLink" + idx} href={repo}>Github Repo</a></span>
+          <span className="repolinks" key={"repo" + idx}>
+            <a key={"repoLink" + idx} href={repo}>Github Repo</a></span>
 
-          {live ? <span className="repolinks" key={"live" + idx}><a key={"liveLink" + idx} href={repo}>Check it live</a></span> : null}
-
+          {live ? <span className="repolinks" key={"live" + idx}>
+            <a key={"liveLink" + idx} href={repo}>Check it live</a></span> : null}
 
         </div>
       </div>
-
-
     )
   })
 }
