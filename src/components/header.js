22a66b24f059fb94b6be8dd429ff073f9d279f54
JoseOgam <jotienoochieng@gmail.com>
2020-03-19 14:39:33 +0300

    header complete

--- src/components/header.js
+++ src/components/header.js
@@ -20,20 +20,28 @@ const Header = () => {
       <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro|Open+Sans&display=swap" rel="stylesheet"/>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
             integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-            crossorigin="anonymous"/>
+            crossOrigin="anonymous"/>
 
-      <div className="main-header">
+      <div className="header-main">
         <div className="dev">
-          <h1>{data.site.siteMetadata.title}</h1>
+          <h1 className="metadata">{data.site.siteMetadata.title}</h1>
           <span className="dot">•</span>
-          <span className="attr">Front-End Developer</span>
+          <span className="attr">Software Engineer</span>
           <span className="dot">•</span>
         </div>
         <div className="icons">
-          <a href='https://github.com/pachicodes' target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-          <a href='https://twitter.com/pachicodes' target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-          <a href='https://dev.to/pachicodes' target="_blank" rel="noopener noreferrer"><i className="fab fa-dev"></i></a>
-          <a href='https://www.linkedin.com/in/pachicodes/' target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+          <a href='' target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"/>
+          </a>
+          <a href='' target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"/>
+          </a>
+          <a href='' target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-dev"/>
+          </a>
+          <a href='' target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"/>
+          </a>
         </div>
         <span className="pages-link">
           <Link to="/">Portfolio</Link>
