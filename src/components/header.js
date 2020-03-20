29a42f1ca686c8785233a00bae760b58905cd17a
JoseOgam <jotienoochieng@gmail.com>
2020-03-20 08:28:43 +0300

    about page fixing

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import { Link, graphql, useStaticQuery } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import "../styles/header.css"
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,44 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import aboutStyle from "../styles/about.styles.css";
 
-const About = () => (
-  <Layout>
+const About = () => {
+  return (
+    <Layout>
+      <h2>My name is Joseph, nice to meet you!</h2>
 
-  </Layout>
-)
+      <div className={aboutStyle.main}>
+        <div className={aboutStyle.izquierda}>
+          <div className={aboutStyle.imagen}>
 
-export default About
+          </div>
+        </div>
+        <div className={aboutStyle.derecha}>
+          <p>I am a self-taught developer in Development.</p>
+          <p>Currently I create websites as a
+            freelancer whenever I have the opportunity.</p>
+          <p>I am active member of the #DEVcommunity online, and I write weekly
+            blog posts about topics I am
+            learning or experiences I had.</p>
+          <p>When I am not coding, you can find me tweeting to #100DaysOfCoding
+            or watching Nat Geo Wild.</p>
+
+          <div className={aboutStyle.socialContainer}>
+            <h3>Some of the tech I like</h3>
+            <img alt="javascript" className="logo" src="./logos/javascript.svg" />
+            <img alt="Gatsby" className="logo" src="./logos/gatsby.svg" />
+            <img alt="GraphQl" className="logo" src="./logos/graphql.svg" />
+            <img alt="React" className="logo" src="./logos/react.svg" />
+            <img alt="nodeJs" className="logo" src="./logos/nodejs-icon.svg" />
+            <img alt="Mongo" className="logo" src="./logos/mongodb.png" />
+            <img alt="Sass" className="logo" src="./logos/sass-1.svg" />
+            <img alt="Laravel" className="logo" src="./logos/laravel.png" />
+            <img alt="Mysql" className="logo" src="./logos/mysql.png" />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
